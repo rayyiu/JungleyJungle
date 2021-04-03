8cd53be4f187001547d47d2b6ba32d223791d8be
rayyiu <rayyiu66@gmail.com>
2021-04-03 06:25:00 +0000

    added admin categories controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,32 @@
 class Admin::CategoriesController < ApplicationController
+
+  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'category created!'
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+    )
+  end
+
 end
+
